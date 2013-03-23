7a9977de6baae422d050cef0259e48ecb1bf870b
Aydin Yakar <yakar@live.com>
2013-03-23 10:00:16 +0200

    Css/Img/Js Files

---
+++ js/jquery.ready.js
@@ -0,0 +1,31 @@
+﻿/**
+*   Author: Aydin YAKAR
+*   Project: jQuery Controls
+*   Version: v0.1
+*   LastMod: 22.03.2013 / 22:21
+*
+*************************/
+
+			$(document).ready(function(){
+
+				$('#calistir').click(function(){
+					if($('#node_name option:selected').val() == '')
+					{
+						$('#komut').addClass('alert-error');
+						$('#komut_mesaji').html('Cihaz seçmelisiniz!');
+					}
+					else if($('#command option:selected').val() == '')
+					{
+						$('#komut').addClass('alert-error');
+						$('#komut_mesaji').html('İşlem seçmelisiniz!');
+					}
+					else
+					{
+						$('#komut').removeClass('alert-error').addClass('alert-success');
+						$('#komut_mesaji').html( $('#node_name option:selected').val() + ' is ' + $('#command option:selected').val() );
+						//$('#vln_form').submit();
+					}
+
+				});
+
+			});
